786c19fafb0119bd6673bdb00bd744aa98a16b0f
achilove <javoxiraka@gmail.com>
2020-02-21 09:21:27 +0500

    feat: what season task is completed

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,29 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(d) {
+  function byMonth(month) {
+    ++month
+    if (3 <= month && month <= 5) {
+      return 'spring';
+    }
+
+    if (6 <= month && month <= 8) {
+      return 'summer';
+    }
+
+    if (9 <= month && month <= 11) {
+      return 'fall';
+    }
+
+    return 'winter';
+  }
+
+
+  if (!d) {
+    return 'Unable to determine the time of year!'
+  }
+  if (!(d instanceof Date) || isNaN(d.getTime())) {
+    throw Error('invalid date')
+  }
+
+  return byMonth(d.getMonth())
+
 };
