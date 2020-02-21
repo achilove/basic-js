649286e89903874c7b2a2b6b03947f1500509547
achilove <javoxiraka@gmail.com>
2020-02-21 09:19:55 +0500

    feat: extended repeater task is comleted

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if(!options.separator){
+        options.separator = '+'
+    }
+    if(!options.additionSeparator){
+        options.additionSeparator = '|'
+    }
+    let addition = '' 
+    for(let i=0; i< options.additionRepeatTimes; i++){
+
+        addition += options.addition 
+        if(i !== options.additionRepeatTimes -1){
+            addition +=options.additionSeparator
+        }
+    }
+    let result =''
+    for(let i=0; i<options.repeatTimes; i++){
+        result += str + addition;
+        if(i !== options.repeatTimes - 1){
+            result += options.separator
+        }
+    }
+
+    if(!options.repeatTimes && !options.additionRepeatTimes){
+        result = str + options.addition
+    }
+
+    return result
 };
   
