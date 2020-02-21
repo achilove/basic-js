c3352f2fdff5c4bc34b56ab065dc05e7e68fe56b
achilove <javoxiraka@gmail.com>
2020-02-21 09:18:47 +0500

    init: BasicJS task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let parsedSampleActivity = parseFloat(sampleActivity);
+  let validator = (isNaN(parsedSampleActivity)
+    || parsedSampleActivity <= 0
+    || parsedSampleActivity > MODERN_ACTIVITY
+    || typeof sampleActivity !== "string");
+  return validator ? false : Math.ceil(Math.log(MODERN_ACTIVITY / parsedSampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 };
