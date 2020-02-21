95e5abbc3fc6630e82ee4e16093e419feb822077
achilove <javoxiraka@gmail.com>
2020-02-21 09:19:23 +0500

    feat: Count cats task is completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  let number = 0;
+  matrix.flat().forEach(el => {
+    if(el === "^^"){
+      ++number 
+    }
+  })
+
+  return number;
 };
