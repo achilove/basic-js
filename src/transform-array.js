85184b66ba04db5cb6159fe1c382b557a995adaa
achilove <javoxiraka@gmail.com>
2020-02-21 09:20:45 +0500

    task: transform array task is completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw Error('arguement is not array')
+    }
+
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--double-next':
+                if (arr[i + 1] !== undefined) {
+                    newArr.push(arr[i + 1]);
+                }
+                break;
+            case '--discard-next':
+                i = i + 1;
+                break;
+            case '--discard-prev':
+                newArr.pop();
+                break;
+            case '--double-prev':
+                if (arr[i - 1] !== undefined) {
+                    newArr.push(arr[i - 1]);
+                }
+                break;
+            default:
+                newArr.push(arr[i]);
+                break;
+
+        }
+
+    }
+    return newArr;
 };
