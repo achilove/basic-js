e39451c998152c53262a502270d78f799cee4f94
achilove <javoxiraka@gmail.com>
2020-02-21 09:19:37 +0500

    feat: dream-team task is completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+
+  if(!Array.isArray(arr)){
+    return false
+  }  
+
+  let teamName = []
+  for(let i=0; i<arr.length; i++){
+      if( typeof(arr[i]) === 'string' ){
+          arr[i] = arr[i].trim()
+          teamName.push(arr[i][0].toUpperCase())
+      }
+  }
+
+  if(teamName.length === 0){
+      return false
+  }
+
+  return teamName.sort().join('')
 };
